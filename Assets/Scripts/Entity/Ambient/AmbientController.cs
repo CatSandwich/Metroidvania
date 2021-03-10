b0b35e36c2f2dc2071edafb073bca21a4d68db72
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2021-03-10 17:25:56 -0500

    Comments

--- Assets/Scripts/Entity/Ambient/AmbientController.cs
+++ Assets/Scripts/Entity/Ambient/AmbientController.cs
@@ -6,33 +6,42 @@ namespace Entity.Ambient
     [RequireComponent(typeof(BoxCollider2D))]
     public class AmbientController : MonoBehaviour
     {
-        //override this to true if entity should listen for nearby player
+        // Override this to true if entity should listen for nearby player
         protected virtual bool ReactsToPlayer => false;
+        // Override this to set a custom radius to react to nearby players
         protected virtual float PlayerReactDistance => 1f;
         
+        // These two lines demonstrate a common pattern you'll see me use a lot. 
+        // There is a public property (property = custom getters and setters) which returns:
+        //     privateBackingField ??= Get the value here
+        // This gets the value the first time it's referenced, then stores it for later use.
         private Transform PlayerTransform => _playerTransform ??= GameObject.FindWithTag("Player").transform;
         private Transform _playerTransform;
         
         public Vector2 StartingVelocity;
         
-        protected Rigidbody2D _rb;
-        protected Animator _animator;
-        protected bool _isReactingToPlayer;
-
+        protected Rigidbody2D RB;
+        protected Animator Animator;
+        protected bool IsReactingToPlayer;
         
-
         public void Start()
         {
-            _rb = GetComponent<Rigidbody2D>();
-            _animator = GetComponent<Animator>();
-            _rb.velocity = StartingVelocity;
+            // Get needed components
+            RB = GetComponent<Rigidbody2D>();
+            Animator = GetComponent<Animator>();
+            
+            // Set starting movement
+            RB.velocity = StartingVelocity;
         }
         
         public void Update()
         {
+            // Handle player reaction
             _checkPlayerReact();
-            if (_rb.velocity.x < 0f) transform.localScale = new Vector3(-1f, 1f, 1f);
-            if (_rb.velocity.x > 0f) transform.localScale = new Vector3(1f, 1f, 1f);
+            
+            // Set which direction the sprite is facing based on movement direction
+            if (RB.velocity.x < 0f) transform.localScale = new Vector3(-1f, 1f, 1f);
+            if (RB.velocity.x > 0f) transform.localScale = new Vector3(1f, 1f, 1f);
         }
         
         // Called when ReactsToPlayer is true and player comes within PlayerReactDistance units from the entity
@@ -41,22 +50,28 @@ namespace Entity.Ambient
         // Called when ReactsToPlayer is true and player stops being within PlayerReactDistance units from the entity
         protected virtual void _reactToPlayerStop(Transform playerTransform) { }
         
+        // Handle player reaction and call appropriate functions
         private void _checkPlayerReact()
         {
+            // If entity does not listen for this event, ignore the rest of this code
             if (!ReactsToPlayer) return;
 
             var distanceToPlayer = (transform.position - PlayerTransform.position).magnitude;
             
-            if (_isReactingToPlayer)
+            if (IsReactingToPlayer)
             {
+                // If player is still within react range return
                 if (distanceToPlayer <= PlayerReactDistance) return;
-                _isReactingToPlayer = false;
+                
+                IsReactingToPlayer = false;
                 _reactToPlayerStop(PlayerTransform);
             }
             else
             {
+                // If player is still out of react range return
                 if (distanceToPlayer > PlayerReactDistance) return;
-                _isReactingToPlayer = true;
+                
+                IsReactingToPlayer = true;
                 _reactToPlayerStart(PlayerTransform);
             }
         }
--- Assets/Scripts/Entity/Ambient/Bird/BirdController.cs
+++ Assets/Scripts/Entity/Ambient/Bird/BirdController.cs
@@ -6,50 +6,67 @@ namespace Entity.Ambient.Bird
 {
     public class BirdController : AmbientController
     {
+        // Must have this line to react to nearby players
         protected override bool ReactsToPlayer => true;
+        // Sets a custom reaction radius
         protected override float PlayerReactDistance => 2f;
+        
+        // Mask (filter) that checks if a given game object is of layer "Terrain"
         private MetroidMask TerrainMask => new MetroidMask("Terrain");
 
+        // Boolean property for whether or not the bird is grounded (sitting on terrain)
+        // Get simply returns the value
+        // Set sets the value then informs the animator and stops vertical movement if applicable
         private bool IsGrounded
         {
             get => _isGrounded;
             set
             { 
                 _isGrounded = value;
-                _animator.SetBool("IsGrounded", _isGrounded);
-                if (_isGrounded) _rb.velocity *= new Vector2(1f, 0f);
+                Animator.SetBool("IsGrounded", _isGrounded);
+                if (_isGrounded) RB.velocity *= new Vector2(1f, 0f);
             }
         }
         private bool _isGrounded;
 
+        // WingSpeed property is used solely to increase the fly animation speed
+        // _wingSpeed is the backing field (properties don't store data, needs a backing field to do so)
+        // Everything is normal except setting the value also informs the animator
         private float WingSpeed
         {
             get => _wingSpeed;
             set
             {
                 _wingSpeed = value;
-                _animator.SetFloat("WingSpeed", _wingSpeed);
+                Animator.SetFloat("WingSpeed", _wingSpeed);
             }
         }
         private float _wingSpeed = 1f;
         
+        // Listen for collisions that would ground the bird
         public void OnCollisionEnter2D(Collision2D other)
         {
+            // Only listen for terrain collisions
             var mask = TerrainMask;
             mask.GO = other.gameObject;
             if (!mask.HasLayer) return;
+            
+            // Only listen for collisions below the bird
             if (!CollisionDirection.CheckDirection(gameObject, other.gameObject, Vector2.down)) return;
 
+            // Remember this property's setter also informs the animator
             IsGrounded = true;
         }
 
+        // This taps into the base class' functionality of running code when the player comes close enough
         protected override void _reactToPlayerStart(Transform playerTransform)
         {
+            // Make the bird fly away
             IsGrounded = false;
             WingSpeed = 2f;
             
             var direction = playerTransform.position.x > transform.position.x ? -1f : 1f;
-            _rb.velocity = new Vector2(5f * direction, 2f);
+            RB.velocity = new Vector2(5f * direction, 2f);
         }
     }
 }
